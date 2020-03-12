72ace1049d5ae39e7f1a96d442fe043b2141f2e7
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-03-12 15:53:31 -0400

    exercises 3 & 4

--- sorting.js
+++ sorting.js
@@ -142,37 +142,35 @@ function qSort(array, start = 0, end = array.length) {
   if (start >= end) {
     return array;
   }
-  //   console.log("start: ", start, "end: ", end);
   const middle = partition(array, start, end);
-  console.log("middle: ", middle);
   array = qSort(array, start, middle);
   array = qSort(array, middle + 1, end);
   return array;
-} /*p*/
-
-/*i*/ /*j*/ testArray = [89, 30, 25, 32, 72];
-//[25, 30, 89, 32, 72]
+}                            
+                 
+const testArray = [32, 25, 30, 89, 72];
+const testArray2 = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1,6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5]
 
 function partition(array, start, end) {
-  console.log(array, start, end);
-  const randomIndex = Math.floor(Math.random() * (end - 1 - start) + start);
-  const pivot = array[randomIndex];
-  let j = start;
-  // if(randomIndex === start) {
-  //     j = start + 1
-  // }
-  for (i = start; i < end - 1; i++) {
+  
+  const pivot = array[start];
+  let j = start + 1;
+  let i = end - 1
+  while(j <= i) {
     if (array[i] <= pivot) {
       swap(array, i, j);
       j++;
     }
+    else {
+        --i
+    }
   }
-  swap(array, end - 1, j);
+  swap(array, start, i);
 
-  return j;
+  return i;
 }
 
-console.log("final:", qSort(testArray, 0, testArray.length));
+//console.log("final:", qSort(testArray2, 0, testArray2.length));
 
 /**
  * Interview Questions
@@ -210,3 +208,47 @@ function bitSort(arr) {
 }
 
 // console.log(bitSort([1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0]));
+
+/*Exercise 4*/
+
+function mSort(arr) {
+    if(arr.length <= 1) {
+        return arr
+    }
+
+    const middle = Math.floor(arr.length/2);
+    let left = arr.slice(0, middle)
+    let right = arr.slice(middle, arr.length)
+
+    left = mSort(left)
+    right = mSort(right)
+
+    
+    return mergeEm(left, right, arr)
+}
+
+function mergeEm (left, right, arr) {
+    let indexLeft = 0
+    let indexRight = 0
+    let indexOutput = 0
+
+    while(indexLeft < left.length && indexRight < right.length){
+        if(left[indexLeft] < right[indexRight]) {
+            arr[indexOutput++] = left[indexLeft++]
+        }
+        else {
+            arr[indexOutput++] = right[indexRight++]
+        }
+    }
+
+    for(let i=indexLeft; i<left.length; i++) {
+        arr[indexOutput++] = left[i]
+    }
+
+    for(let i=indexRight; i < right.length; i++) {
+        arr[indexOutput++] = right[i]
+    }
+    return arr
+}
+
+//console.log(mSort(testArray2))
